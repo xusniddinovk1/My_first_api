6cd2a8279f6dbf678d64a5c7aae1107981efa4bb
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-04-20 16:46:19 +0500

    initial commit

--- my_api/views.py
+++ my_api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-data_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
